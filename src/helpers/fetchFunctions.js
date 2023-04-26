8c20953e336196a2c27063e781254d2652510461
Bruno <bruno_bury@hotmail.com>
2023-04-26 19:19:19 -0300

    test: adicionado testes na função fetchProductsList, requisito 1

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,10 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async () => {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$QUERY');
+  console.log('response:', response);
+  const data = await response.json();
+  return data;
 };
+fetchProductsList();
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,21 +1,23 @@
 import './mocks/fetchSimulator';
 import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
-
+// jest.mock('node-fetch')
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function')
   });
-
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async  () => {
+    expect(await fetchProductsList('computador')).not.toThrow()
   });
-
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {     
+    const url = ('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    expect(fetch).toHaveBeenCalledWith(url) 
   });
-
-  // it('...', () => {
-  // });
+  it('Verifica se estrutura de dados do retorno da função está correto ', async () => {
+    expect(await fetchProductsList('computador')).toEqual(computadorSearch)
+  });
+  it('Verifica se a função sem argumento retorna mensagem de erro', async () => {
+    expect(await fetchProductsList()).toThrow('Termo de busca não informado')
+  });
 });
