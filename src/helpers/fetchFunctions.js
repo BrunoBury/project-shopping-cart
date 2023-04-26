9bbf952fee8141fba36e3f76fd1c02e504bc88b3
Bruno <bruno_bury@hotmail.com>
2023-04-26 20:22:19 -0300

    feat: implementando e função fetchProductsList

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,10 +2,13 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = async () => {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$QUERY');
-  console.log('response:', response);
-  const data = await response.json();
-  return data;
+export const fetchProductsList = async (endPoint) => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${endPoint}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    return 'Termo de busca não informado';
+  }
 };
-fetchProductsList();
+console.log(fetchProductsList('computador'));
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,23 +1,27 @@
 import './mocks/fetchSimulator';
 import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
+import fetchSimulator from './mocks/fetchSimulator';
 // jest.mock('node-fetch')
+
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
     expect(typeof fetchProductsList).toBe('function')
   });
   it('fetch é chamado ao executar fetchProductsList', async  () => {
-    expect(await fetchProductsList('computador')).not.toThrow()
+    await fetchProductsList('computador')
+    expect(fetch).toHaveBeenCalled()
   });
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {     
     const url = ('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    expect(fetch).toHaveBeenCalledWith(url) 
+    await fetchProductsList('computador')
+    expect(fetch).toHaveBeenCalledWith(url)
   });
   it('Verifica se estrutura de dados do retorno da função está correto ', async () => {
     expect(await fetchProductsList('computador')).toEqual(computadorSearch)
   });
   it('Verifica se a função sem argumento retorna mensagem de erro', async () => {
-    expect(await fetchProductsList()).toThrow('Termo de busca não informado')
+    expect(await fetchProductsList()).toBe('Termo de busca não informado')
   });
 });
