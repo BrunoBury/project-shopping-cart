8d6e738ffe6fc948b32f8a52c0b102e8fca8033d
Bruno <bruno_bury@hotmail.com>
2023-04-27 18:34:39 -0300

    feat: implementando lista de produtos, requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const productsEL = document.querySelector('.products');
+
+const productsApi = await fetchProductsList('computador');
+productsApi.forEach((element) => {
+  productsEL.appendChild(createProductElement(element));
+});
